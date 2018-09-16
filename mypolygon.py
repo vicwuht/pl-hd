625d76382511776a3b362b6fc9d25e6a8e023ae5
vic <w80u10@163.com>
2018-09-16 23:32:06 +0800

    turtle 测试

--- mypolygon.py
+++ mypolygon.py
@@ -4,10 +4,10 @@ world = TurtleWorld()
 bob = Turtle()
 print(bob)
 
-# fd(bob,100)
-# lt(bob)
-# pu(bob)
-# fd(bob,100)
+fd(bob,100)
+lt(bob)
+pu(bob)
+fd(bob,100)
 
 for i in range(4):
     fd(bob,100)
