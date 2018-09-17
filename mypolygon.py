6364ac21cdc71e7bc19fd2c6c7062c028dabcc47
vic <w80u10@163.com>
2018-09-17 23:44:36 +0800

    turtle 测试

--- mypolygon.py
+++ mypolygon.py
@@ -1,16 +1,43 @@
 from TurtleWorld import *
+import math
 
 world = TurtleWorld()
 bob = Turtle()
-print(bob)
+bob.delay = 0.01
+# print(bob)
+# fd(bob,100)
+# lt(bob)
+# pu(bob)
+# fd(bob,100)
 
-fd(bob,100)
-lt(bob)
-pu(bob)
-fd(bob,100)
 
-for i in range(4):
-    fd(bob,100)
-    lt(bob)
+# def square(t, length):
+#     for i in range(4):
+#         fd(t, length)
+#         lt(t)
+#
+#
+# square(bob, 120)
+
+
+# def polygon(t, length, n):
+#     for i in range(n):
+#         fd(t, length)
+#         lt(t, 360/n)
+#
+#
+# polygon(bob, 120, 5)
+
+
+def circle(t, r):
+    n=100
+    for i in range(n):
+        length = math.pi* 2 * r / n
+        fd(t, length)
+        lt(t, 360/n)
+
+
+circle(bob, 100)
+
 
 wait_for_user()
