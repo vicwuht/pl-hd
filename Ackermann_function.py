188d0113b1040f9020bb27d36eda204cea1f956b
vic <w80u10@163.com>
2018-09-26 23:25:22 +0800

    turtle 测试

---
+++ Ackermann_function.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10000000)
+
+
+def ack(m, n):
+    if m == 0:
+        return n+1
+    elif n == 0:
+        return ack(m-1, 1)
+    else:
+        return ack(m-1, ack(m, n-1))
+
+
+print(ack(22, 33))
