149eda502b5b159354d7b4e70c9be5fe0e4f875c
vic <w80u10@163.com>
2020-04-04 15:44:36 +0800

    笨办法学python习题7

---
+++ HARD_WAY/ex7.py
@@ -0,0 +1,33 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went")
+print("."*10)
+
+#给end1赋值
+end1 = "C"
+#给end2赋值
+end2 = "h"
+#给end3赋值
+end3 = "e"
+#给end4赋值
+end4 = "e"
+#给end5赋值
+end5 = "s"
+#给end6赋值
+end6 = "e"
+#给end7赋值
+end7 = "B"
+#给end8赋值
+end8 = "u"
+#给end9赋值
+end9 = "r"
+#给end10赋值
+end10 = "g"
+#给end11赋值
+end11 = "e"
+#给end12赋值
+end12 = "r"
+#打印cheese，并且以空格作为结束符
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+#打印burger
+print(end7 + end8 + end9 + end10 + end11 + end12)
