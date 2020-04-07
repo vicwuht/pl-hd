07842b9c8187a4d7f1e83b14414c4d80318a4360
vic <w80u10@163.com>
2020-04-08 00:52:16 +0800

    0407

---
+++ HARD_WAY/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+tall = input()
+print("How much weight are you ?", end = ' ')
+weight = input()
+
+print("so ,you're {} old, {} tall and {} heavy".format(age, tall, weight))
