3bd2f1002a76b1bfad671ea1afd3cdb6510816ef
vic <w80u10@163.com>
2020-04-14 23:44:53 +0800

    0414

---
+++ HARD_WAY/ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you?")
+tall = input("How tall are you?")
+weight = input("How much weight are you ?")
+
+print("so ,you're {} old, {} tall and {} heavy".format(age, tall, weight))
---
+++ HARD_WAY/ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script,first,second,third = argv
+
+print("your script is called:", script)
+print("your first variable is :", first)
+print("your second variable is :", second)
+print("your third variable is :", third)
+
+nice = input("get your argument:")
+print("your argument is :", nice)
