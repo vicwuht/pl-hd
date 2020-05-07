34ed583e67ecd4074c66d3f30fcaeff35730a17a
vic <w80u10@163.com>
2020-05-07 23:28:16 +0800

    200507

---
+++ HARD_WAY/ex32.py
@@ -0,0 +1,15 @@
+counts = [1, 2, 3, 4, 5]
+for number in counts:
+    print(f"This is count {number}")
+
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+for fruit in fruits:
+    print("A fruit of type: %s" % fruit)
+
+change = ['apples', 1, 'oranges', 2, 'pears', 3]
+for i in change:
+    print(f"I got {i}")
+
+elements = range(0, 6)
+for i in elements:
+    print(f"element is : {i}")
